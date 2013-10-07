1078ca69cc1abfb68ed3c2c855bb1a28941673c8
Grégory Paul <paulgreg@gmail.com>
2013-10-07 22:03:07 +0200

    Bugfix about unsafe characters in targets name

--- server/launcher.js
+++ server/launcher.js
@@ -1,7 +1,22 @@
 var fs = require('fs');
+var _ = require('underscore');
+
 fs.readFile(__dirname+'/config.json', 'utf8', function (err, data) {
-    if (err) { return console.log('No config file found :', err); }
-    var monitor = require('./monitor')(JSON.parse(data));
-    monitor.launch();
+    if (err) { 
+        return console.log('No config file found :', err);
+    }
+
+    try {
+        var config = JSON.parse(data);
+
+        _.each(config.targets, function(target) {
+            target.name = target.name.replace(/[^\w]/g, '-'); // Replace unsafe characters by dashes
+        });
+
+        var monitor = require('./monitor')(config);
+        monitor.launch();
+    } catch(e) {
+        console.log('Error while parsing configuration or during monitoring', e);
+    }
 });
 
