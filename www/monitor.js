ddc84ec700d5bdd25d50020fbd51246a0fa8e81b
Grégory PAUL <paulgreg@gmail.com>
2013-10-07 16:58:38 +0200

    Handle flapping on web UI / Introduced instable/orange state

--- www/monitor.js
+++ www/monitor.js
@@ -8,7 +8,7 @@ var Monitor = (function(undefined) {
         for(var i = 0; i < daysToShow; i++) {
             var hours = [];
             for(var h = startAt; h < stopAt; h++) {
-                hours.push({ 'status': 'unknown' });
+                hours.push({ 'status': 'unknown', 'statusCodes': [] });
             }
             days.push({ 'hours': hours });
         }
@@ -18,18 +18,42 @@ var Monitor = (function(undefined) {
     var fillTimeTable = function(config, now, timetable, uptime) {
         var day = 86400000; // 24 * 60 * 60 * 1000;
         _.each(uptime.results, function(result) {
-            var up = result.status === 200 ? 'up' : 'down';
             var ts = result.timestamp;
             var dayDiff = parseInt((now - ts) / day, 10);
             if (dayDiff < config.server.daysToShow) {
                 var tsDate = new Date(ts);
                 var hour = tsDate.getHours();
                 var idxToRemove = (config.server.startAt) ? config.server.startAt : 0;
-                var oldStatus = timetable.days[dayDiff].hours[(hour-idxToRemove)].status;
-                if (oldStatus !== 'down') {
-                    timetable.days[dayDiff].hours[(hour-idxToRemove)].status = up;
-                }
+                var cell = timetable.days[dayDiff].hours[(hour-idxToRemove)];
+                cell.statusCodes.push(result.status);
             }
+        });
+        _.each(timetable.days, function(day) {
+            _.each(day.hours, function(hour) {
+                if (hour.statusCodes.length > 0) {
+                    var previousFails = 0;
+                    var totalFails = 0;
+                    var moreFailsThanFlapping = false;
+                    for(var i = 0; i < hour.statusCodes.length; i++) {
+                        if (hour.statusCodes[i] === 200) {
+                            previousFails = 0;
+                        } else {
+                            totalFails++;
+                            previousFails++;
+                            if (previousFails >= config.server.flapping) {
+                                moreFailsThanFlapping = true;
+                            }
+                        }
+                    }
+                    if (moreFailsThanFlapping === true) {
+                        hour.status = 'down';
+                    } else if (totalFails > 0) {
+                        hour.status = 'instable';
+                    } else {
+                        hour.status = 'up';
+                    }
+                }
+            });
         });
     };
 
